8e99f43bee37d1bbcf02b9c4bc98e0aad277eaf0
Stephanie Altman <slappyhoursound@gmail.com>
2020-10-27 15:34:03 -0400

    using collections

--- Program.cs
+++ Program.cs
@@ -10,7 +10,18 @@ namespace csharpcollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] daysOfWeek = {
+                "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
+            };
+
+             Console.WriteLine(" which day u want b?");
+             Console.Write("(Monday = 1 , etc. ) >");
+             int iDay = int.Parse(Console.ReadLine()) - 1;
+
+             string chosenDay = daysOfWeek[iDay];
+             Console.WriteLine($"Tha day is {chosenDay}");
         }
     }
 }
+
+// stuff i already know 
